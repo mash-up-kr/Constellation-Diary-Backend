537432304f5f39ae2d8cd7000d5baa8dee5851bc
HyeranShin <hyeran9712@naver.com>
2019-12-03 22:11:13 +0900

    Initial commit

---
+++ src/main/java/com/mashup/byeolbyeol/ByeolbyeolApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.byeolbyeol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeolbyeolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByeolbyeolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mashup/byeolbyeol/ByeolbyeolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashup.byeolbyeol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByeolbyeolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
