f378903ff34c2e1b23125459ec5ffd9422850593
HyeranShin <hyeran9712@naver.com>
2019-12-11 00:29:27 +0900

    [#3][FEAT] User 도메인 레이어 생성

---
+++ src/main/java/com/kancho/byeolbyeol/user/domain/User.java
@@ -0,0 +1,40 @@
+package com.kancho.byeolbyeol.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer constellationsId;
+
+    private String deviceId;
+
+    private Boolean horoscopeAlarmFlag;
+
+    private String questionTime;
+
+    private Boolean questionAlarmFlag;
+
+    @Builder
+    public User(Integer constellationsId, String deviceId, Boolean horoscopeAlarmFlag, String questionTime, Boolean questionAlarmFlag) {
+        this.constellationsId = constellationsId;
+        this.deviceId = deviceId;
+        this.horoscopeAlarmFlag = horoscopeAlarmFlag;
+        this.questionTime = questionTime;
+        this.questionAlarmFlag = questionAlarmFlag;
+    }
+}
