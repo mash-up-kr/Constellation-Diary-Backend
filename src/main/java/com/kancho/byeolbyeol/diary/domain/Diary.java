b414befeb1f93883806b499fedd09309bc589ecb
HyeranShin <hyeran9712@naver.com>
2019-12-12 10:38:36 +0900

    [#3][FEAT] Diary 도메인 레이어 생성

---
+++ src/main/java/com/kancho/byeolbyeol/diary/domain/Diary.java
@@ -0,0 +1,37 @@
+package com.kancho.byeolbyeol.diary.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity(name = "diaries")
+public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long usersId;
+
+    private String date;
+
+    private String title;
+
+    private String content;
+
+    @Builder
+    public Diary(Long userId, String date, String title, String content) {
+        this.usersId = userId;
+        this.date = date;
+        this.title = title;
+        this.content = content;
+    }
+}
