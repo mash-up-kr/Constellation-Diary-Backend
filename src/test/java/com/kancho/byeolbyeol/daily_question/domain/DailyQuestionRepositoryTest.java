b4fe1146f58bf7693d8aeb5a5620a37c887c962c
HyeranShin <hyeran9712@naver.com>
2019-12-12 11:29:38 +0900

    [#3][TEST] DailyQuestion 생성, DailyQuestionRepository 저장하고 불러오기

---
+++ src/test/java/com/kancho/byeolbyeol/daily_question/domain/DailyQuestionRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.kancho.byeolbyeol.daily_question.domain;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DailyQuestionRepositoryTest {
+
+    @Autowired
+    DailyQuestionRepository dailyQuestionRepository;
+
+    @After
+    public void cleanup() {
+        dailyQuestionRepository.deleteAll();
+    }
+
+    @Test
+    public void 오늘의질문_저장하고_불러오기() {
+        //given
+        dailyQuestionRepository.save(DailyQuestion.builder()
+                .date("2019년 12월 12일")
+                .content("오늘 가장 감사한 사람은 누구인가요?")
+                .build());
+
+        //when
+        List<DailyQuestion> dailyQuestionList = dailyQuestionRepository.findAll();
+
+        //then
+        DailyQuestion dailyQuestion = dailyQuestionList.get(0);
+        assertThat(dailyQuestion.getDate(), is("2019년 12월 12일"));
+        assertThat(dailyQuestion.getContent(), is("오늘 가장 감사한 사람은 누구인가요?"));
+    }
+}
