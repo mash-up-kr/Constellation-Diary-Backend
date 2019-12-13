ce3a3f6bcfd0512bbb017430bb1d785e1f5f1e40
HyeranShin <hyeran9712@naver.com>
2019-12-13 21:13:31 +0900

    [#3][TEST] ConstellationRepository 물병자리 조회하기

---
+++ src/test/java/com/kancho/byeolbyeol/constellation/domain/ConstellationRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.kancho.byeolbyeol.constellation.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConstellationRepositoryTest {
+
+    @Autowired
+    ConstellationRepository constellationRepository;
+
+    @Test
+    public void 물병자리_조회하기() {
+        List<Constellation> constellationList = constellationRepository.findAll();
+        Constellation aquarius = constellationList.get(0);
+
+        assertThat(aquarius.getId(), is(1));
+        assertThat(aquarius.getName(), is("물병자리"));
+        assertThat(aquarius.getDate(), is("1월 20일~2월18일"));
+        assertThat(aquarius.getDescription(), is("상식이나 인간관계에 얽매이지 않고 나아가는 자유로운 개혁자"));
+    }
+}
