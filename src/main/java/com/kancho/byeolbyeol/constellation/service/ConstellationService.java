8f840a057c696593c9e9b3fb0cb16a9a8a3b0628
HyeranShin <hyeran9712@naver.com>
2019-12-15 01:46:55 +0900

    [#4][FEAT] 별자리 리스트(이미지 URL) 조회 API

---
+++ src/main/java/com/kancho/byeolbyeol/constellation/service/ConstellationService.java
@@ -0,0 +1,23 @@
+package com.kancho.byeolbyeol.constellation.service;
+
+import com.kancho.byeolbyeol.constellation.domain.ConstellationRepository;
+import com.kancho.byeolbyeol.constellation.dto.response.ResConstellationImageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ConstellationService {
+
+    private final ConstellationRepository constellationRepository;
+
+    public List<ResConstellationImageDto> getConstellationImageList() {
+        return constellationRepository.findAll()
+                .stream()
+                .map(constellation -> new ResConstellationImageDto(constellation))
+                .collect(Collectors.toList());
+    }
+}
