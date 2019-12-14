9245a1d4c39449f3c969d4985a0b686be1f3567e
HyeranShin <hyeran9712@naver.com>
2019-12-14 19:23:37 +0900

    [FEAT] Swagger Config 추가

---
+++ src/main/java/com/kancho/byeolbyeol/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.kancho.byeolbyeol.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .directModelSubstitute(Object.class, java.lang.Void.class)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kancho.byeolbyeol"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("별별일기")
+                .description("별자리 운세 일기 앱 * 별별일기 * API 문서")
+                .contact(new Contact("kancho", "https://github.com/mash-up-kr/Constellation-Diary-Backend", "hyeran.dev@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .version("1.0")
+                .build();
+    }
+}
