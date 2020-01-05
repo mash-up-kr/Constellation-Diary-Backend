4287eb440ae729d959b8f79f7d217a1743ad706d
korea8378 <korea8378@naver.com>
2020-01-05 16:38:12 +0900

    [#27][FEAT] ehcache 삭제

--- src/main/java/com/kancho/byeolbyeol/ByeolbyeolApplication.java
+++ src/main/java/com/kancho/byeolbyeol/ByeolbyeolApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ByeolbyeolApplication {
 
     public static void main(String[] args) {
