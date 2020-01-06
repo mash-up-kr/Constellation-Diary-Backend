bf05eb015331a87b071d914983b2732f574a8dd1
korea8378 <korea8378@naver.com>
2020-01-07 03:35:44 +0900

    [#52][FEAT] 운세, 데일리 질문 날짜 타입 변경 (#53)

---
+++ src/main/java/com/kancho/byeolbyeol/common/LocalDatePersistenceConverter.java
@@ -0,0 +1,22 @@
+package com.kancho.byeolbyeol.common;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Converter(autoApply = true)
+public class LocalDatePersistenceConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate entityValue) {
+        return Date.from(entityValue.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date databaseValue) {
+        return Instant.ofEpochMilli(databaseValue.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+}
