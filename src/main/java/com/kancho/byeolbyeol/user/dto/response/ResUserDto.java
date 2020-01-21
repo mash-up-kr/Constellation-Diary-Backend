122433734a3d052850e46e3d2eef9b710a841aae
korea8378 <korea8378@naver.com>
2020-01-22 01:26:26 +0900

    [#83][FEAT] ResUserDto - horoscopeTime 추가 (#84)

--- src/main/java/com/kancho/byeolbyeol/user/dto/response/ResUserDto.java
+++ src/main/java/com/kancho/byeolbyeol/user/dto/response/ResUserDto.java
@@ -14,17 +14,20 @@ public class ResUserDto {
     private String timeZone;
     private Boolean horoscopeAlarmFlag;
     private Boolean questionAlarmFlag;
-    private String questionTime;
+    private LocalTime horoscopeTime;
+    private LocalTime questionTime;
 
     @Builder
     private ResUserDto(Long id, String userId, String constellation, String timeZone,
-                       Boolean horoscopeAlarmFlag, Boolean questionAlarmFlag, LocalTime questionTime) {
+                       Boolean horoscopeAlarmFlag, Boolean questionAlarmFlag,
+                       LocalTime horoscopeTime, LocalTime questionTime) {
         this.id = id;
         this.userId = userId;
         this.constellation = constellation;
         this.timeZone = timeZone;
         this.horoscopeAlarmFlag = horoscopeAlarmFlag;
         this.questionAlarmFlag = questionAlarmFlag;
-        this.questionTime = questionTime.toString();
+        this.horoscopeTime = horoscopeTime;
+        this.questionTime = questionTime;
     }
 }
