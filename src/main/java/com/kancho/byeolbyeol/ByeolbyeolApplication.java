66f840da8e4366e46906cc9a13db2855a1ec9646
korea8378 <korea8378@naver.com>
2020-01-28 08:19:12 +0000

    [#97][FEAT] 비동기 어노테이션 추가

--- src/main/java/com/kancho/byeolbyeol/ByeolbyeolApplication.java
+++ src/main/java/com/kancho/byeolbyeol/ByeolbyeolApplication.java
@@ -3,8 +3,10 @@ package com.kancho.byeolbyeol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ByeolbyeolApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/kancho/byeolbyeol/user/application/EmailService.java
+++ src/main/java/com/kancho/byeolbyeol/user/application/EmailService.java
@@ -1,5 +1,8 @@
 package com.kancho.byeolbyeol.user.application;
 
+import org.springframework.scheduling.annotation.Async;
+
 public interface EmailService {
+    @Async
     void sendAuthenticationNumberMail(String to, String subject, String authenticationNumber);
 }
