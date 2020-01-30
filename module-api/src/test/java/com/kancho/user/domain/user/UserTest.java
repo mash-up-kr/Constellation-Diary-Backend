0a2512067af4a80df6ca47ed44121b4621286980
korea8378 <korea8378@naver.com>
2020-01-30 21:17:36 +0000

    [#124][MODIFY] 불필요한 Import 정리

--- module-api/src/test/java/com/kancho/user/domain/user/UserTest.java
+++ module-api/src/test/java/com/kancho/user/domain/user/UserTest.java
@@ -1,10 +1,7 @@
 package com.kancho.user.domain.user;
 
-import com.kancho.common.constant.ReqTimeZone;
 import com.kancho.constellation.Constellation;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalTime;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
