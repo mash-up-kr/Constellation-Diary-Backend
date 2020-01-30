a11b50c8d9c3fd879544b10a15114ac1ca1573fb
korea8378 <korea8378@naver.com>
2020-01-30 16:36:00 +0000

    [#111][FEAT] API Common Test Case 추가

---
+++ module-common/src/test/java/com/kancho/daily_question/DailyQuestionTest.java
@@ -0,0 +1,21 @@
+package com.kancho.daily_question;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DailyQuestionTest {
+
+    @Test
+    public void 오늘의질문_생성() {
+        DailyQuestion dailyQuestion = DailyQuestion.builder()
+                .date(LocalDate.now())
+                .content("오늘 가장 감사한 사람은 누구인가요?")
+                .build();
+
+        assertThat(dailyQuestion.getContent(), is("오늘 가장 감사한 사람은 누구인가요?"));
+    }
+}
