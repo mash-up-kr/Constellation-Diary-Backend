b0141fb20cffc26d354992d0e5b67cf1059471ed
korea8378 <korea8378@naver.com>
2020-02-01 02:02:12 +0900

    [#128][FEAT] 운세 푸시 알람 구현 (#134)
    * [#128][FEAT] 운세 푸시 알람 로직 추가
    
    * [#128][FEAT] 패키지 구조 변경(푸시로 통합)
    
    * [#128][FEAT] FCM에 푸시 하는 로직을 비동기로 처리
    
    * [#128][FEAT] 매분마다 스케줄러 동작하게 적용

---
+++ module-push/src/main/kotlin/com/kancho/push/application/PushService.kt
@@ -0,0 +1,26 @@
+package com.kancho.push.application
+
+import com.kancho.user.User
+import com.kancho.user.UserRepository
+import org.springframework.stereotype.Service
+import java.time.LocalTime
+
+@Service
+class PushService(private val notificationService: NotificationService,
+                  private val userRepository: UserRepository) {
+    fun sendHoroscopePushAlarm(nowTime: LocalTime) {
+
+        val users: MutableList<User> = userRepository.findByHoroscopeAlarmFlagAndHoroscopeTime(true, nowTime)
+
+        var tokens: MutableList<String> = mutableListOf()
+        for (user in users) {
+            if (tokens.size == 500) {
+                notificationService.send(tokens, "별별일기", "오늘의 운세를 확인해보세요.")
+                tokens = mutableListOf()
+            } else {
+                tokens.add(user.fcmToken)
+            }
+
+        }
+    }
+}
---
+++ module-push/src/main/kotlin/com/kancho/push/controller/PushController.kt
@@ -0,0 +1,12 @@
+package com.kancho.push.controller
+
+import com.kancho.push.application.PushService
+import org.springframework.stereotype.Component
+import java.time.LocalTime
+
+@Component
+class PushController(private val pushService: PushService) {
+
+    fun sendHoroscopePushAlarm(nowTime: LocalTime) = pushService.sendHoroscopePushAlarm(nowTime)
+
+}
