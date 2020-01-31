adaabd50e33517164a64fa9219cef964408fb1b0
korea8378 <korea8378@naver.com>
2020-01-31 16:59:28 +0000

    [#128][FEAT] FCM에 푸시 하는 로직을 비동기로 처리

--- module-push/src/main/kotlin/com/kancho/push/application/NotificationService.kt
+++ module-push/src/main/kotlin/com/kancho/push/application/NotificationService.kt
@@ -1,6 +1,9 @@
 package com.kancho.push.application
 
+import org.springframework.scheduling.annotation.Async
+
 interface NotificationService {
+    @Async
     fun send(tokens: List<String>, title: String, message: String)
 }
 
