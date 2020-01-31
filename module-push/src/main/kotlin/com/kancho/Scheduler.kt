501082dfb1a62437d9d159c43ac7433a1f9f543c
korea8378 <korea8378@naver.com>
2020-01-31 17:08:47 +0000

    [#129][FEAT] Profile을 통하여 스케줄러 분기 처리 적용

--- module-push/src/main/kotlin/com/kancho/Scheduler.kt
+++ module-push/src/main/kotlin/com/kancho/Scheduler.kt
@@ -1,6 +1,7 @@
 package com.kancho
 
 import com.kancho.push.controller.PushController
+import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.LocalTime
@@ -8,11 +9,13 @@ import java.time.LocalTime
 @Component
 class Scheduler(val pushController: PushController) {
 
+    @Profile("horoscope")
     @Scheduled(cron = "0 */1 * * * *")
     fun startHoroscopeJob() {
         pushController.sendHoroscopePushAlarm(LocalTime.now())
     }
 
+    @Profile("question")
     @Scheduled(cron = "0 */1 * * * *")
     fun startQuestionJob() {
         pushController.sendDailyQuestionPushAlarm(LocalTime.now())
