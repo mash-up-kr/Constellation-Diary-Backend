9d8a958d3810cd452176eeb3168d6486d434d53c
korea8378 <korea8378@naver.com>
2020-02-08 23:44:42 +0900

    [#140][FEAT] 다수의 일기 삭제 기능 (#154)
    * [#140][FEAT] 일기 리스트 삭제 API 추가
    
    * [#140][FEAT] Exception Getter 추가
    
    * [#140][MODIFY] Repository Query Naming 변경

---
+++ module-api/src/main/java/com/kancho/daily/dto/ReqDiariesDto.java
@@ -0,0 +1,17 @@
+package com.kancho.daily.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqDiariesDto {
+
+    @NotEmpty
+    List<Long> diaryIds;
+}
---
+++ module-api/src/main/java/com/kancho/daily/exception/IsEmptyDiariesException.java
@@ -0,0 +1,23 @@
+package com.kancho.daily.exception;
+
+import com.kancho.exception.BaseException;
+import com.kancho.exception.ErrorModel;
+import org.springframework.http.HttpStatus;
+
+public class IsEmptyDiariesException extends BaseException {
+    public IsEmptyDiariesException() {
+        this(HttpStatus.BAD_REQUEST);
+    }
+
+    private IsEmptyDiariesException(HttpStatus httpStatus) {
+        this(4004, httpStatus);
+    }
+
+    private IsEmptyDiariesException(int code, HttpStatus httpStatus) {
+        super(ErrorModel.builder()
+                .code(code)
+                .httpStatus(httpStatus)
+                .massage("Is Empty Diaries")
+                .build());
+    }
+}
