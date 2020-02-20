72c55bb4598cd97a9f2893797514793da9f9270c
korea8378 <korea8378@naver.com>
2020-02-20 07:10:38 +0000

    [#162][MODIFY] async Bean Name 추가

--- module-push/src/main/kotlin/com/kancho/push/application/NotificationService.kt
+++ module-push/src/main/kotlin/com/kancho/push/application/NotificationService.kt
@@ -3,7 +3,7 @@ package com.kancho.push.application
 import org.springframework.scheduling.annotation.Async
 
 interface NotificationService {
-    @Async
+    @Async("pushSenderThreadPool")
     fun send(tokens: List<String>, title: String, body: String)
 }
 
