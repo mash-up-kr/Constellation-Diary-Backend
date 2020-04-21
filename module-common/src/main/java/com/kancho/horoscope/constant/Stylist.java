8f1091dc7cbe16ca4acb2c9cdfd0ac35ab3440c9
korea8378 <korea8378@naver.com>
2020-04-21 14:52:29 +0900

    [#187][FEAT] 스타일리스트 단어 길이 짧게 변경

--- module-common/src/main/java/com/kancho/horoscope/constant/Stylist.java
+++ module-common/src/main/java/com/kancho/horoscope/constant/Stylist.java
@@ -7,15 +7,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Stylist {
-    METAL_MATERIAL_COMPONENTS("금속재료소품"),
-    SILVER_RING("은색반지"),
+    METAL_MATERIAL_COMPONENTS("금속"),
+    SILVER_RING("은반지"),
     BOOK("책"),
-    BOTTLE_OF_WATER("물이 담긴 병"),
+    BOTTLE_OF_WATER("물병"),
     LEATHER_BAGS("가죽가방"),
     WALLET("지갑"),
     SCARF("스카프"),
     GLASSES("안경"),
-    WOOD_PROPS("나무로 된 소품"),
+    WOOD_PROPS("나무소품"),
     NECKLACE("목걸이"),
     RAW_FLOWERS("생화"),
     BADGE("뱃지"),
